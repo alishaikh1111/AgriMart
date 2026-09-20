cbd508224c23d5d5b15b8ccd91b13028a1183552
Ali Shaikh <alishaikh41305@gmail.com>
2026-09-21 00:53:02 +0530

    Update server port for Render

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // JSON data receive karne ke liye
 app.use(express.json());
